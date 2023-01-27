193983f629f6edd6b4bd65f3c61a39cb59778c63
zmy1213 <81918271+zmy1213@users.noreply.github.com>
2023-01-27 13:19:40 +0800

    Create UnionAndFInd.cpp

---
+++ UnionAndFInd.cpp
@@ -0,0 +1,92 @@
+#include<iostream>
+#include<unordered_map>
+#include<unordered_set>
+#include<vector>
+#include<stack>
+using namespace std;
+
+template<typename T>
+struct Element
+{
+	T val;
+	Element() = default;
+	Element(T value):val(value){}
+};
+
+template<typename T>
+class UnionFindSet
+{
+private:
+	unordered_map<T, Element<T>*> elementMap;
+	unordered_map<Element<T>*, Element<T>*>fatherMap;
+	unordered_map<Element<T>*, int> sizeMap;
+
+public:
+	UnionFindSet(vector<T>& list)
+	{
+		for (auto& value : list)
+		{
+			Element<T>* element = new Element<T>(value);
+			elementMap.emplace(value, element);
+			fatherMap.emplace(element, element);
+			sizeMap.emplace(element, 1);
+		}
+	}
+
+	Element<T>* findHead(Element<T>* element)
+	{
+		stack<Element<T>*> path;
+		while (element != (fatherMap.find(element))->second)
+		{
+			path.push(element);
+			element = fatherMap.find(element)->second;
+		}
+
+		while (!path.empty())
+		{
+			fatherMap[path.top()] = element;
+			path.pop();
+		}
+		return element;
+	}
+
+	bool isSameSet(T a, T b)
+	{
+		if ((elementMap.find(a) != elementMap.end()) && (elementMap.find(b) != elementMap.end()))
+		{
+			return findHead(elementMap.find(a)->second) == findHead(elementMap.find(b)->second);
+		}
+		return false;
+	}
+
+	void Union(T a, T b)
+	{
+		if (elementMap.count(a) && elementMap.count(b))
+		{
+			Element<T> *aF = findHead(elementMap.find(a)->second);
+			Element<T> *bF = findHead(elementMap.find(b)->second);
+
+			if (aF != bF)
+			{
+				Element<T> *big = sizeMap.find(aF)->second >= sizeMap.find(bF)->second ? aF : bF;
+				Element<T> *small = big == aF ? bF : aF;
+				fatherMap[small] = big;
+				sizeMap.emplace(big, sizeMap.find(aF)->second + sizeMap.find(bF)->second);
+				sizeMap.erase(small);
+			}
+		}
+	}
+};
+
+int main()
+{
+	vector<int> nums{ 1,2,3,4,5,6,7,8,9 };
+	UnionFindSet<int> s(nums);
+	s.Union(1, 2);
+	s.Union(1, 3);
+	s.Union(1, 7);
+	bool res = s.isSameSet(1,5);
+
+	cout << res << endl;
+	
+}
